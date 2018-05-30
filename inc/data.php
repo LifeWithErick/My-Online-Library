f1fb851ff1a0c5c2c2f37fcfabcd2eb3b2a7e243
Erick Seva <39140429+LifeWithErick@users.noreply.github.com>
2018-05-30 18:55:24 -0400

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,179 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The Fault In Our Stars",
+	"img" => "img/media/Book1.png",
+    "genre" => "Young Adult Novel",
+    "format" => "Paperback",
+    "year" => 2012,
+    "category" => "Books",
+    "authors" => [
+        "John Green"
+    ],
+    "publisher" => "Penguin",
+    "isbn" => '9781594137907'
+];
+$catalog[102] = [
+    "title" => "The Curious Incident Of The Dog In The Night-Time",
+    "img" => "img/media/Book2.jpg",
+    "genre" => "Mystery",
+    "format" => "Paperback",
+    "year" => 2003,
+    "category" => "Books",
+    "authors" => [
+        "Mark Haddon"
+    ],
+    "publisher" => "Doubleday",
+    "isbn" => '9781101911617'
+];
+$catalog[103] = [
+    "title" => "Le Petit Nicolas a des ennuis",
+    "img" => "img/media/Book3.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 1964,
+    "category" => "Books",
+    "authors" => [
+        "René Goscinny",
+        "Jean-Jacques Sempé"
+
+    ],
+    "publisher" => "Jean-Jacques Sempé",
+    "isbn" => '978-0201485677'
+];
+$catalog[104] = [
+    "title" => "Le Petit Prince",
+    "img" => "img/media/Book4.jpg",
+    "genre" => "Fable",
+    "format" => "Paperback",
+    "year" => 1943,
+    "category" => "Books",
+    "authors" => [
+        "Antoine de Saint-Exupéry"
+    ],
+    "publisher" => "Antoine de Saint-Exupéry",
+    "isbn" => '978-0822594222'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Finding Dory",
+    "img" => "img/media/Movie1.jpg",
+    "genre" => "Comedy-drama/Adventure",
+    "format" => "DVD",
+    "year" => 2016,
+    "category" => "Movies",
+    "director" => "Andrew Stanton, Angus MacLane",
+    "writers" => [
+        "Andrew Stanton",
+        "Angus MacLane"
+    ],
+    "stars" => [
+        "Ellen DeGeneres",
+        "Albert Brooks",
+        "Ed O'Neill",
+        "Kaitlin Olson"
+    ]
+];
+$catalog[202] = [
+    "title" => "Black Panther",
+    "img" => "img/media/Movie2.jpg",
+    "genre" => "Fantasy/Science fiction film",
+    "format" => "Blu-ray",
+    "year" => 2018,
+    "category" => "Movies",
+    "director" => "Ryan Coogler",
+    "writers" => [
+        "Ryan Coogler"
+    ],
+    "stars" => [
+        "Chadwick Boseman",
+        "Michael B. Jordan",
+        "Lupita Nyong'o",
+        "Danai Gurira",
+        "Daniel Kaluuya",
+        "Forest Whitaker"
+    ]
+];
+$catalog[203] = [
+    "title" => "La Ch'tite famille",
+    "img" => "img/media/Movie3.jpg",
+    "genre" => "Comedy",
+    "format" => "Blu-ray",
+    "year" => 2018,
+    "category" => "Movies",
+    "director" => "Dany Boon",
+    "writers" => [
+        "Dany Boon"
+    ],
+    "stars" => [
+        "Dany Boon",
+        "Line Renaud",
+        "Laurence Arné",
+        "Valérie Bonneton",
+        "Guy Lecluyse",
+        "François Berléand"
+    ]
+];
+$catalog[204] = [
+    "title" => "Papertowns",
+    "img" => "img/media/Movie4.jpg",
+    "genre" => "Drama/Mystery",
+    "format" => "DVD",
+    "year" => 2015,
+    "category" => "Movies",
+    "director" => "Jake Schreier",
+    "writers" => [
+        "Jake Schreier"
+    ],
+    "stars" => [
+        "Nat Wolff",
+        "Cara Delevingne",
+        "Austin Abrams",
+        "Justice Smith",
+        "Halston Sage",
+        "Jaz Sinclair",
+        "Cara Buono",
+        "Meg Crosbie",
+        "Josiah Cerio",
+        "Hannah Alligood"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Friends",
+    "img" => "img/media/Music1.jpg",
+    "genre" => "Pop",
+    "format" => "MP3",
+    "year" => 2018,
+    "category" => "Music",
+    "artist" => "Marshmello & Anne-Marie"
+];
+$catalog[302] = [
+    "title" => "Alone",
+    "img" => "img/media/Music2.jpg",
+    "genre" => "Future bass, Dance/electronic",
+    "format" => "MP3",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Marshmello"
+];
+$catalog[303] = [
+    "title" => "Handwritten",
+    "img" => "img/media/Music3.jpg",
+    "genre" => "Pop music, Rock music, Blues",
+    "format" => "MP3",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Shawn Mendes"
+];
+$catalog[304] = [
+    "title" => "Dua Lipa",
+    "img" => "img/media/Music4.jpeg",
+    "genre" => "Pop",
+    "format" => "MP3",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Dua Lipa"
+];
+?> 
